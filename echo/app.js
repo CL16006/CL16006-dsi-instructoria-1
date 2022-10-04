ce7a1de613211d09b141362aba2cc4c2d24a279d
CL16006 <calderonlopez678@gmail.com>
2022-10-04 15:18:33 -0600

    programando echo con ts

---
+++ echo/app.js
@@ -0,0 +1,5 @@
+//obteniendo el parametro
+var parametro1 = process.argv[2];
+console.log(parametro1);
+// para ejecutarlo usar el comando: npm start [parametro]
+//Por ejemplo--------->>>>>>>>  npm start "haciendo echo desde ts"
---
+++ echo/app.ts
@@ -0,0 +1,6 @@
+//obteniendo el parametro
+let parametro1: string=process.argv[2];
+
+console.log(parametro1);
+// para ejecutarlo usar el comando: npm start [parametro]
+//Por ejemplo--------->>>>>>>>  npm start "haciendo echo desde ts"
